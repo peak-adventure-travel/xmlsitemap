3450639636d38bbd73a5f771c03ed017c868e59c
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-05-21 22:10:46 +0300

    Issue #2249723 by andrei.dincu: Start XmlSitemapGenerator service

---
+++ lib/Drupal/xmlsitemap/XmlSitemapGenerator.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\xmlsitemap\XmlSitemapGenerator.
+ */
+
+namespace Drupal\xmlsitemap;
+
+
+/**
+ * XmlSitemap generator service.
+ */
+class XmlSitemapGenerator implements XmlSitemapGeneratorInterface {
+
+  
+}
---
+++ lib/Drupal/xmlsitemap/XmlSitemapGeneratorInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\xmlsitemap\XmlSitemapGeneratorInterface.
+ */
+
+namespace Drupal\xmlsitemap;
+
+/**
+ * Provides an interface defining a XmlSitemapGenerator service.
+ */
+interface XmlSitemapGeneratorInterface {
+  
+}
