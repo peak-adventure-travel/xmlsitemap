698419a9ee7eef146a9af02db9fa1373b91fea90
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-05-21 23:54:19 +0300

    Issue #2249723 by andrei.dincu: Put all functions from xmlsitemap.generate.inc into XmlSitemapGenerator service

--- lib/Drupal/xmlsitemap/XmlSitemapGeneratorInterface.php
+++ lib/Drupal/xmlsitemap/XmlSitemapGeneratorInterface.php
@@ -7,9 +7,133 @@
 
 namespace Drupal\xmlsitemap;
 
+use Drupal\Core\Language\LanguageInterface;
+
 /**
  * Provides an interface defining a XmlSitemapGenerator service.
  */
 interface XmlSitemapGeneratorInterface {
+
+  /**
+   * Given an internal Drupal path, return the alias for the path.
+   *
+   * This is similar to drupal_get_path_alias(), but designed to fetch all alises
+   * at once so that only one database query is executed instead of several or
+   * possibly thousands during sitemap generation.
+   *
+   * @param string $path
+   *   An internal Drupal path.
+   * @param Drupal\Core\Language\LanguageInterface $language
+   *   A language code to use when looking up the paths.
+   */
+  public function getPathAlias($path, $language);
+
+  /**
+   * Perform operations before rebuilding the sitemap.
+   */
+  public function regenerateBefore();
+
+  /**
+   * Get how much memory was used
+   *
+   * @param bool $start
+   *
+   */
+  public function getMemoryUsage($start = FALSE);
+
+  /**
+   * Calculate the optimal PHP memory limit for sitemap generation.
+   *
+   * This function just makes a guess. It does not take into account
+   * the currently loaded modules.
+   */
+  public function getOptimalMemoryLimit();
+
+  /**
+   * Calculate the optimal memory level for sitemap generation.
+   *
+   * @param $new_limit
+   *   An optional PHP memory limit in bytes. If not provided, the value of
+   *   getOptimalMemoryLimit() will be used.
+   */
+  public function setMemoryLimit($new_limit = NULL);
+
+  /**
+   * Generate one page (chunk) of the sitemap.
+   *
+   * @param $sitemap
+   *   An unserialized data array for an XML sitemap.
+   * @param $page
+   *   An integer of the specific page of the sitemap to generate.
+   */
+  public function generatePage(stdClass $sitemap, $page);
+
+  /**
+   * Generate one chunk of the sitemap.
+   *
+   * @param $sitemap
+   *   An unserialized data array for an XML sitemap.
+   * @param XMLSitemapWriter $writer
+   *   XML writer object
+   * @param $page
+   *   An integer of the specific page of the sitemap to generate.
+   */
+  public function generateChunk(stdClass $sitemap, XMLSitemapWriter $writer, $chunk);
+
+  /**
+   * Generate the index sitemap.
+   *
+   * @param $sitemap
+   *   An unserialized data array for an XML sitemap.
+   */
+  public function generateIndex(stdClass $sitemap);
+
+  /**
+   * Batch information callback for regenerating the sitemap files.
+   *
+   * @param $smids
+   *   An optional array of XML sitemap IDs. If not provided, it will load all
+   *   existing XML sitemaps.
+   */
+  public function regenerateBatch(array $smids = array());
+
+  /**
+   * Batch callback; generate all pages of a sitemap.
+   */
+  public function regenerateBatchGenerate($smid, array &$context);
+
+  /**
+   * Batch callback; generate the index page of a sitemap.
+   */
+  public function regenerateBatchGenerateIndex($smid, array &$context);
+
+  /**
+   * Batch callback; sitemap regeneration finished.
+   */
+  public function regenerateBatchFinished($success, $results, $operations, $elapsed);
+
+  /**
+   * Batch information callback for rebuilding the sitemap data.
+   */
+  public function rebuildBatch(array $entities, $save_custom = FALSE);
+
+  /**
+   * Batch callback; clear sitemap links for entites.
+   */
+  public function rebuildBatchClear(array $entities, $save_custom, &$context);
+
+  /**
+   * Batch callback; fetch and add the sitemap links for a specific entity.
+   */
+  public function rebuildBatchFetch($entity, &$context);
+
+  /**
+   * Batch callback; sitemap rebuild finished.
+   */
+  public function rebuildBatchFinished($success, $results, $operations, $elapsed);
   
+  /**
+   * Get all rebuldable link types
+   */
+  function getRebuildableLinkTypes();
 }
