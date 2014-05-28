dbfe736532d63be21b8ceacf148b2d720aa97200
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-05-28 20:36:27 +0300

    Issue #2249723 by andrei.dincu: Create XmlSitemapCustomListController class to list all custom links

---
+++ xmlsitemap_custom/lib/Drupal/xmlsitemap_custom/Controller/XmlSitemapCustomListController.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\xmlsitemap_custom\Controller\XmlSitemapCustomListController.
+ */
+
+namespace Drupal\xmlsitemap_custom\Controller;
+
+/**
+ * Builds the list table for all custom links.
+ */
+class XmlSitemapCustomListController {
+
+  public function content() {
+    $header = array(
+      'loc' => array('data' => t('Location'), 'field' => 'loc', 'sort' => 'asc'),
+      'priority' => array('data' => t('Priority'), 'field' => 'priority'),
+      'changefreq' => array('data' => t('Change frequency'), 'field' => 'changefreq'),
+      'language' => array('data' => t('Language'), 'field' => 'language'),
+      'operations' => array('data' => t('Operations')),
+    );
+
+    $rows = array();
+    $destination = drupal_get_destination();
+
+    $query = db_select('xmlsitemap');
+    $query->fields('xmlsitemap');
+    $query->condition('type', 'custom');
+    $query->extend('Drupal\Core\Database\Query\PagerSelectExtender')->limit(50);
+    $query->extend('Drupal\Core\Database\Query\TableSortExtender')->orderByHeader($header);
+    $result = $query->execute();
+
+    foreach ($result as $link) {
+      $row = array();
+      $row['loc'] = l($link->loc, $link->loc);
+      $row['priority'] = number_format($link->priority, 1);
+      $row['changefreq'] = $link->changefreq ? drupal_ucfirst(xmlsitemap_get_changefreq($link->changefreq)) : t('None');
+      if (isset($header['language'])) {
+        $row['language'] = \Drupal::moduleHandler()->invoke('locale', 'language_name', $link->language);
+      }
+      $operations = array();
+      $operations['edit'] = xmlsitemap_get_operation_link('admin/config/search/xmlsitemap/custom/edit/' . $link->id, array('title' => t('Edit'), 'modal' => TRUE));
+      $operations['delete'] = xmlsitemap_get_operation_link('admin/config/search/xmlsitemap/custom/delete/' . $link->id, array('title' => t('Delete'), 'modal' => TRUE));
+      $row['operations'] = array(
+        'data' => array(
+          '#theme' => 'links',
+          '#links' => $operations,
+          '#attributes' => array('class' => array('links', 'inline')),
+        ),
+      );
+      $rows[] = $row;
+    }
+
+    // @todo Convert to tableselect
+    $build['xmlsitemap_custom_table'] = array(
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+      '#empty' => t('No custom links available.') . ' ' . l(t('Add custom link'), 'admin/config/search/xmlsitemap/custom/add', array('query' => $destination)),
+    );
+    $build['xmlsitemap_custom_pager'] = array('#theme' => 'pager');
+
+    return $build;
+  }
+
+}
