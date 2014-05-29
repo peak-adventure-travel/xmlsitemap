ab87932c7ab91bca73a0923c58854ed4c83c4cab
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-05-29 16:13:24 +0300

    Issue #2249723 by andrei.dincu: XmlSitemapCustomAddForm check if link is duplicate in validateForm()

--- xmlsitemap_custom/lib/Drupal/xmlsitemap_custom/Form/XmlSitemapCustomAddForm.php
+++ xmlsitemap_custom/lib/Drupal/xmlsitemap_custom/Form/XmlSitemapCustomAddForm.php
@@ -24,8 +24,6 @@ class XmlSitemapCustomAddForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, array &$form_state) {
-    //module_load_include('inc', 'xmlsitemap', 'xmlsitemap.admin');
-    //_xmlsitemap_set_breadcrumb('admin/config/search/xmlsitemap/custom');
     \Drupal::moduleHandler()->loadInclude('xmlsitemap', 'inc', 'xmlsitemap.admin');
     $link = array();
     $link += array(
@@ -101,30 +99,21 @@ class XmlSitemapCustomAddForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, array &$form_state) {
-    /* $link = &$form_state['values'];
+    $link = &$form_state['values'];
 
-      // Make sure we trim and normalize the path first.
-      $link['loc'] = trim($link['loc']);
-      $link['loc'] = \Drupal::service('path.alias_manager')->getPathByAlias($link['loc'], $link['language']);
-      // Test anonymous user access to the custom link paths.
-      xmlsitemap_switch_user(0);
-      $menu_item = menu_get_item($link['loc']);
-      xmlsitemap_restore_user();
-
-      // Since the menu item access results are cached, manually check the current path.
-      $anonymous_user = new AnonymousUserSession();
-      if ($menu_item && strpos($link['loc'], 'admin/config/search/xmlsitemap/custom') === 0 && !$anonymous_user->hasPermission('administer xmlsitemap')) {
-      $menu_item['access'] = FALSE;
-      }
-
-      if (db_query_range("SELECT 1 FROM {xmlsitemap} WHERE type <> 'custom' AND loc = :loc AND status = 1 AND access = 1 AND language IN (:languages)", 0, 1, array(':loc' => $link['loc'], ':languages' => array(LanguageInterface::LANGCODE_NOT_SPECIFIED, $link['language'])))->fetchField()) {
-      form_set_error('loc', t('There is already an existing link in the sitemap with the path %link.', array('%link' => $link['loc'])));
-      }
-      elseif (empty($menu_item['access']) && !is_readable('./' . $link['loc'])) {
-      // @todo Harden this file exists check to make sure we can't link to files
-      // like .htaccess.
-      form_set_error('loc', t('The custom link %link is either invalid or it cannot be accessed by anonymous users.', array('%link' => $link['loc'])));
-      } */
+    // Make sure we trim and normalize the path first.
+    $link['loc'] = trim($link['loc']);
+    $link['loc'] = \Drupal::service('path.alias_manager')->getPathByAlias($link['loc'], $link['language']);
+    $query = db_select('xmlsitemap');
+    $query->fields('xmlsitemap');
+    $query->condition('type', 'custom');
+    $query->condition('loc', $link['loc']);
+    $query->condition('status',1 );
+    $query->condition('access',1);
+    $result = $query->execute()->fetchAssoc();
+    if ($result != FALSE) {
+      \Drupal::formBuilder()->setErrorByName('loc', $form_state, t('There is already an existing link in the sitemap with the path %link.', array('%link' => $link['loc'])));
+    }
     parent::validateForm($form, $form_state);
   }
 
@@ -135,7 +124,8 @@ class XmlSitemapCustomAddForm extends FormBase {
     $link = $form_state['values'];
     xmlsitemap_link_save($link);
     drupal_set_message(t('The custom link for %loc was saved.', array('%loc' => $link['loc'])));
-    $form_state['redirect'] = 'admin/config/search/xmlsitemap/custom';
+
+    $form_state['redirect_route']['route_name'] = 'xmlsitemap_custom.list';
   }
 
 }
