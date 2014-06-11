4b64beff1eabfe4d4cd2dab3c244067ae4f98185
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-06-11 18:59:46 +0300

    Issue #2249723 by andrei.dincu: Add xmlsitemap_engines_test.routing.yml file and controller for it

---
+++ xmlsitemap_engines/tests/modules/xmlsitemap_engines_test/src/Controller/XmlSitemapEnginesTestController.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\xmlsitemap\Controller\XmlSitemapController.
+ */
+
+namespace Drupal\xmlsitemap_engines_test\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class XmlSitemapEnginesTestController extends ControllerBase {
+
+  public function render() {
+    if (empty($_GET['sitemap']) || !valid_url($_GET['sitemap'])) {
+      watchdog('xmlsitemap', 'No valid sitemap parameter provided.', array(), WATCHDOG_WARNING);
+      // @todo Remove this? Causes an extra watchdog error to be handled.
+      throw new NotFoundHttpException();
+    }
+    else {
+      watchdog('xmlsitemap', 'Recieved ping for @sitemap.', array('@sitemap' => $_GET['sitemap']));
+    }
+    return new Response('', 200);
+  }
+
+}
