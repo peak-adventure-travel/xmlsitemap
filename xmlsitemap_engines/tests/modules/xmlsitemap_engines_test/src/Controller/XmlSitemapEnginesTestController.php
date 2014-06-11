91ac966de5262faaf52d6f06a982843663a18e70
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-06-12 01:35:31 +0300

    Issue #2249723 by andrei.dincu: Replace valid_url() with UrlHelper::isValid()

--- xmlsitemap_engines/tests/modules/xmlsitemap_engines_test/src/Controller/XmlSitemapEnginesTestController.php
+++ xmlsitemap_engines/tests/modules/xmlsitemap_engines_test/src/Controller/XmlSitemapEnginesTestController.php
@@ -10,11 +10,12 @@ namespace Drupal\xmlsitemap_engines_test\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\Component\Utility\UrlHelper;
 
 class XmlSitemapEnginesTestController extends ControllerBase {
 
   public function render() {
-    if (empty($_GET['sitemap']) || !valid_url($_GET['sitemap'])) {
+    if (empty($_GET['sitemap']) || !UrlHelper::isValid($_GET['sitemap'])) {
       watchdog('xmlsitemap', 'No valid sitemap parameter provided.', array(), WATCHDOG_WARNING);
       // @todo Remove this? Causes an extra watchdog error to be handled.
       throw new NotFoundHttpException();
