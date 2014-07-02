cf2360b14a0d635384318289b4a991e9a734e54b
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-07-02 07:26:13 +0300

    Issue #2249723 by andrei.dincu: Update XmlSitemapRebuildForm::submitForm, XmlSitemapRebuildForm::buildForm and delete XmlSitemapRebuildForm::validateForm

--- src/Form/XmlSitemapRebuildForm.php
+++ src/Form/XmlSitemapRebuildForm.php
@@ -40,7 +40,7 @@ class XmlSitemapRebuildForm extends ConfigFormBase {
     // Build a list of rebuildable link types.
     module_load_include('generate.inc', 'xmlsitemap');
     $rebuild_types = xmlsitemap_get_rebuildable_link_types();
-    $rebuild_types = array_combine($rebuild_types,$rebuild_types);
+    $rebuild_types = array_combine($rebuild_types, $rebuild_types);
     $form['entities'] = array(
       '#type' => 'select',
       '#title' => t("Select which link types you would like to rebuild"),
@@ -50,16 +50,12 @@ class XmlSitemapRebuildForm extends ConfigFormBase {
       '#default_value' => \Drupal::config('xmlsitemap.settings')->get('rebuild_needed') || !\Drupal::state()->get('developer_mode') ? $rebuild_types : array(),
       '#access' => \Drupal::state()->get('developer_mode'),
     );
+    $form['save_custom'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Save and restore any custom inclusion and priority links.'),
+      '#default_value' => TRUE,
+    );
     return parent::buildForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, array &$form_state) {
-    // Check that the chunk size will not create more than 1000 chunks.
-
-    parent::validateForm($form, $form_state);
   }
 
   /**
@@ -67,7 +63,10 @@ class XmlSitemapRebuildForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, array &$form_state) {
     // Save any changes to the frontpage link.
-    xmlsitemap_link_save(array('type' => 'frontpage', 'id' => 0, 'loc' => ''));
+    module_load_include('generate.inc', 'xmlsitemap');
+    $batch = xmlsitemap_rebuild_batch($form_state['values']['entities'], $form_state['values']['save_custom']);
+    batch_set($batch);
+    $form_state['redirect'] = 'admin/config/search/xmlsitemap';
 
     parent::submitForm($form, $form_state);
   }
