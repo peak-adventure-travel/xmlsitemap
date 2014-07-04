0266ac32e2f0da6c2df740bee3b46852029ed364
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-07-05 01:35:26 +0300

    Issue #2249723 by andrei.dincu: XmlSitemapUserFunctionalTest pass all tests

--- src/Tests/XmlSitemapUserFunctionalTest.php
+++ src/Tests/XmlSitemapUserFunctionalTest.php
@@ -14,7 +14,7 @@ class XmlSitemapUserFunctionalTest extends XmlSitemapTestHelper {
 
   protected $normal_user;
   protected $accounts = array();
-  public static $modules = array('xmlsitemap', 'user', 'node');
+  public static $modules = array('xmlsitemap', 'user', 'node', 'system');
 
   public static function getInfo() {
     return array(
@@ -27,10 +27,16 @@ class XmlSitemapUserFunctionalTest extends XmlSitemapTestHelper {
   public function setUp($modules = array()) {
     parent::setUp();
 
+    // allow anonymous user to view user profiles
+    $user_role = entity_load('user_role', DRUPAL_ANONYMOUS_RID);
+    $user_role->grantPermission('access user profiles');
+    $user_role->save();
+
     // set xmlsitemap_entity_user state variable to TRUE to add user links into sitemap
     \Drupal::state()->set('xmlsitemap_entity_user', TRUE);
+    \Drupal::state()->set('xmlsitemap_entity_user_bundle_user', TRUE);
     // Save the user settings before creating the users.
-    xmlsitemap_link_bundle_settings_save('user', 'user', array('status' => 1, 'priority' => 0.5));
+    xmlsitemap_link_bundle_settings_save('user', 'user', array('status' => 0, 'priority' => 0.5));
 
     // Create the users
     $this->admin_user = $this->drupalCreateUser(array('administer users', 'administer permissions', 'administer xmlsitemap'));
@@ -43,17 +49,17 @@ class XmlSitemapUserFunctionalTest extends XmlSitemapTestHelper {
 
   public function testBlockedUser() {
     $this->drupalLogin($this->admin_user);
-    $this->assertSitemapLinkVisible('user', $this->normal_user->id());
+    $this->assertSitemapLinkNotVisible('user', $this->normal_user->id());
 
     // Mark the user as blocked.
     $edit = array(
-      'status' => 0,
+      'xmlsitemap[status]' => 1,
     );
 
     // This will pass when http://drupal.org/node/360925 is fixed.
     $this->drupalPostForm('user/' . $this->normal_user->id() . '/edit', $edit, t('Save'));
     $this->assertText('The changes have been saved.');
-    $this->assertSitemapLinkNotVisible('user', $this->normal_user->id());
+    $this->assertSitemapLinkVisible('user', $this->normal_user->id());
   }
 
 }
