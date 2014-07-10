0425020425f466707c19d725a6e03f983e55ef5d
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-07-10 03:26:59 +0300

    Issue #2249723 by andrei.dincu: XmlSitemapMenuFunctionalTest passes all tests

--- src/Tests/XmlSitemapMenuFunctionalTest.php
+++ src/Tests/XmlSitemapMenuFunctionalTest.php
@@ -16,7 +16,7 @@ class XmlSitemapMenuFunctionalTest extends XmlSitemapTestHelper {
 
   protected $normal_user;
   protected $menu_items = array();
-  public static $modules = array('xmlsitemap', 'menu_link', 'menu_ui');
+  public static $modules = array('node','xmlsitemap', 'menu_link', 'menu_ui');
 
   public static function getInfo() {
     return array(
@@ -29,6 +29,34 @@ class XmlSitemapMenuFunctionalTest extends XmlSitemapTestHelper {
   public function setUp() {
     parent::setUp();
 
+    if ($this->profile != 'standard') {
+      $this->drupalCreateContentType(array('type' => 'page', 'name' => 'Basic page', 'settings' => array(
+          // Set proper default options for the page content type.
+          'node' => array(
+            'options' => array('promote' => FALSE),
+            'submitted' => FALSE,
+          ),
+      )));
+      $this->drupalCreateContentType(array('type' => 'article', 'name' => 'Article'));
+    }
+
+    // allow anonymous user to administer menu links
+    $user_role = entity_load('user_role', DRUPAL_ANONYMOUS_RID);
+    $user_role->grantPermission('administer menu');
+    $user_role->grantPermission('access content');
+    $user_role->save();
+
+    $bundles = \Drupal::entityManager()->getAllBundleInfo();
+    \Drupal::config('xmlsitemap.settings')->set('xmlsitemap_entity_menu_link', TRUE);
+    \Drupal::config('xmlsitemap.settings')->set('xmlsitemap_entity_menu', TRUE);
+    foreach ($bundles['menu_link'] as $bundle_id => $bundle) {
+      \Drupal::config('xmlsitemap.settings')->set('xmlsitemap_entity_menu_link_bundle_' . $bundle_id, TRUE);
+    }
+    foreach ($bundles['menu'] as $bundle_id => $bundle) {
+      \Drupal::config('xmlsitemap.settings')->set('xmlsitemap_entity_menu_bundle_' . $bundle_id, TRUE);
+    }
+    \Drupal::config('xmlsitemap.settings')->save();
+
     $this->admin_user = $this->drupalCreateUser(array('administer menu', 'administer xmlsitemap'));
     $this->normal_user = $this->drupalCreateUser(array('access content'));
   }
@@ -37,14 +65,18 @@ class XmlSitemapMenuFunctionalTest extends XmlSitemapTestHelper {
     $this->drupalLogin($this->admin_user);
 
     $edit = array(
-      'title' => $this->randomName(),
-      'menu_name' => drupal_strtolower($this->randomName()),
+      'label' => $this->randomName(),
+      'id' => drupal_strtolower($this->randomName()),
       'xmlsitemap[status]' => '1',
       'xmlsitemap[priority]' => '1.0',
     );
     $this->drupalPostForm('admin/structure/menu/add', $edit, 'Save');
 
-    $menu = Menu::load($edit['menu_name']);
+    \Drupal::config('xmlsitemap.settings')->set('xmlsitemap_entity_menu_bundle_' . $edit['id'], TRUE);
+    \Drupal::config('xmlsitemap.settings')->set('xmlsitemap_entity_menu_link_bundle_' . $edit['id'], TRUE);
+    \Drupal::config('xmlsitemap.settings')->save();
+
+    $menu = Menu::load($edit['id']);
 
     $this->clickLink('Add link');
     $edit = array(
@@ -54,6 +86,20 @@ class XmlSitemapMenuFunctionalTest extends XmlSitemapTestHelper {
       'xmlsitemap[priority]' => 'default',
     );
     $this->drupalPostForm(NULL, $edit, 'Save');
+  }
+
+  public function tearDown() {
+    $bundles = \Drupal::entityManager()->getAllBundleInfo();
+    \Drupal::config('xmlsitemap.settings')->delete('xmlsitemap_entity_menu_link');
+    \Drupal::config('xmlsitemap.settings')->delete('xmlsitemap_entity_menu');
+    foreach ($bundles['menu_link'] as $bundle_id => $bundle) {
+      \Drupal::config('xmlsitemap.settings')->delete('xmlsitemap_entity_menu_link_bundle_' . $bundle_id);
+    }
+    foreach ($bundles['menu'] as $bundle_id => $bundle) {
+      \Drupal::config('xmlsitemap.settings')->delete('xmlsitemap_entity_menu_bundle_' . $bundle_id);
+    }
+
+    parent::tearDown();
   }
 
 }
