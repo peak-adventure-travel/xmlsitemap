08cd7bc5c957fec796f295d84e4c93bc32c2479c
Andrei - Marius Dincu <andrei.dincu@gmail.com>
2014-07-12 02:17:15 +0300

    Issue #2249723 by andrei.dincu: Add French and English languages in XmlSitemapI18nWebTestCase

--- src/Tests/XmlSitemapI18nWebTestCase.php
+++ src/Tests/XmlSitemapI18nWebTestCase.php
@@ -8,6 +8,7 @@
 namespace Drupal\xmlsitemap\Tests;
 
 use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Language\Language;
 
 /**
  * Common base test class for XML sitemap internationalization tests.
@@ -15,7 +16,7 @@ use Drupal\Core\Language\LanguageInterface;
 abstract class XmlSitemapI18nWebTestCase extends XmlSitemapTestBase {
 
   protected $admin_user;
-  public static $modules = array('language', 'xmlsitemap', 'locale');
+  public static $modules = array('language', 'xmlsitemap', 'node', 'locale', 'content_translation', 'system');
 
   /**
    * Set up an administrative user account and testing keys.
@@ -24,13 +25,26 @@ abstract class XmlSitemapI18nWebTestCase extends XmlSitemapTestBase {
     // Call parent::setUp() allowing test cases to pass further modules.
     parent::setUp();
 
-    // Add predefined language and reset the locale cache.
-    /* require_once DRUPAL_ROOT . '/includes/locale.inc';
-      locale_add_language('fr', NULL, NULL, LANGUAGE_LTR, '', 'fr');
-      drupal_language_initialize();
-      variable_set('language_negotiation', LOCALE_LANGUAGE_NEGOTIATION_URL_PREFIX);
-     */
+    $this->admin_user = $this->drupalCreateUser(array('administer languages', 'access administration pages', 'administer site configuration', 'administer xmlsitemap', 'access content'));
+    $this->drupalLogin($this->admin_user);
 
+    if (!\Drupal::languageManager()->getLanguage('fr')) {
+      // Add a new language.
+      $language = new Language(array(
+        'id' => 'fr',
+        'name' => 'French',
+      ));
+      language_save($language);
+    }
+
+    if (!\Drupal::languageManager()->getLanguage('en')) {
+      // Add a new language.
+      $language = new Language(array(
+        'id' => 'en',
+        'name' => 'English',
+      ));
+      language_save($language);
+    }
     // Create the two different language-context sitemaps.
     $previous_sitemaps = entity_load_multiple('xmlsitemap');
     foreach ($previous_sitemaps as $previous_sitemap) {
